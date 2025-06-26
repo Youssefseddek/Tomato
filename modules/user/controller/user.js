6f94d9f0c5e78233f78658ae9f09cce3ad874d98
youssef <ym658089@gmail.com>
2025-06-26 17:09:53 +0300

    update 2

--- modules/user/controller/user.js
+++ modules/user/controller/user.js
@@ -6,14 +6,45 @@ export const getUsers = async (req, res) => {
   res.json({ message: "user", user });
 };
 
-export const addImage = async (req, res) => {
-  if (!req.file) {
-    res.status(200).json({ message: "upload your image" });
-  } else {
-    console.log(req.file);
+// export const addImage = async (req, res) => {
+//   if (!req.file) {
+//     res.status(200).json({ message: "upload your image" });
+//   } else {
+//     console.log(req.file);
 
-    const { secure_url } = await cloudinary.uploader.upload(req.file.path, { folder: "tomatoConnect/user" });
-    console.log(secure_url);
+//     const { secure_url } = await cloudinary.uploader.upload(req.file.path, { folder: "tomatoConnect/user" });
+//     console.log(secure_url);
 
+//   }
+// };
+
+
+// update user profile
+export const editProfile = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { name, phone } = req.body;
+    
+    // name or phone is required
+    if (!name && !phone) {
+      return res.status(400).json({ message: "Name or phone is required" });
+    }
+    const updateProfile = await userModel.findByIdAndUpdate(
+      id,
+      { name, phone},
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Profile updated successfully",
+      updateProfile,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+      stack: error.stack,
+    });
   }
-};
+
+}
--- modules/user/user.router.js
+++ modules/user/user.router.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import * as userController from "./controller/user.js";
 import { HME, myMulter, validationTypes } from "../../services/multer.js";
+import auth from "../../middleWare/authentication.js";
 
 const router = Router();
 
 router.get("/", userController.getUsers);
 
 
-router.post("/addImage",myMulter(validationTypes.image).single('image'),HME, userController.addImage);
+// router.post("/addImage",myMulter(validationTypes.image).single('image'),HME, userController.addImage);
+
+// edit user profile
+router.put("/editProfile",auth(), myMulter(validationTypes.image).single('image'), HME, userController.editProfile);
 
 export default router;
